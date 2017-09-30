a76f3f6c1d5c6fcb314c80204a53e9682f651f5f
zc <123456>
2017-09-30 10:15:35 +0800

    测试

---
+++ application/common/validate/Repair.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace  app\common\validate;
+use think\Validate;
+
+class Repair extends  Validate{
+    // 验证规则
+    protected $rule = [
+        ['name','require','报修人必须填写'],
+        ['tel','require','电话必须填写'],
+        ['address','require','地址必须填写'],
+        ['problem','require','事故详情必须填写'],
+        ['status','require','必须填写'],
+
+
+    ];
+}
---
+++ application/common/validate/User.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2017/9/29
+ * Time: 19:36
+ */
+namespace app\common\validate;
+use think\Validate;
+
+class User extends Validate{
+
+}
